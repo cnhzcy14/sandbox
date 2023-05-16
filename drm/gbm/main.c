4da75e8bd58bd7ea29825452f950345b2a860d98
cnhzcy14 <cnhzcy14@gmail.com>
2023-05-16 11:07:43 +0800

    update

--- drm/gbm/main.c
+++ drm/gbm/main.c
@@ -48,132 +48,117 @@ void main(void) {                                                       \
 }                                                                       \
 "
 
-check_extensions(void)
-{
-   const char *client_extensions = eglQueryString(EGL_NO_DISPLAY, EGL_EXTENSIONS);
-
-   if (!client_extensions) {
-      // No client extensions string available
-      abort();
-   }
-   if (!strstr(client_extensions, "EGL_KHR_platform_gbm")) {
-      abort();
-   }
-   printf("========= %s\n", client_extensions);
-}
-
 int32_t
-main (int32_t argc, char* argv[])
+main(int32_t argc, char *argv[])
 {
-   bool res;
-   check_extensions();
-   int32_t fd = open ("/dev/dri/renderD128", O_RDWR);
-   assert (fd > 0);
+    bool res;
+    int32_t fd = open("/dev/dri/card0", O_RDWR);
+    assert(fd > 0);
 
-   struct gbm_device *gbm = gbm_create_device (fd);
-   assert (gbm != NULL);
+    struct gbm_device *gbm = gbm_create_device(fd);
+    assert(gbm != NULL);
 
-   /* setup EGL from the GBM device */
-   EGLDisplay egl_dpy = eglGetPlatformDisplay (EGL_PLATFORM_GBM_KHR, gbm, NULL);
-   assert (egl_dpy != NULL);
+    PFNEGLGETPLATFORMDISPLAYEXTPROC eglGetPlatformDisplayEXT =
+        (PFNEGLGETPLATFORMDISPLAYEXTPROC)eglGetProcAddress("eglGetPlatformDisplayEXT");
+    /* setup EGL from the GBM device */
+    EGLDisplay egl_dpy = eglGetPlatformDisplayEXT(EGL_PLATFORM_GBM_KHR, gbm, NULL);
+    assert(egl_dpy != NULL);
 
-   res = eglInitialize (egl_dpy, NULL, NULL);
-   assert (res);
+    res = eglInitialize(egl_dpy, NULL, NULL);
+    assert(res);
 
-   const char *egl_extension_st = eglQueryString (egl_dpy, EGL_EXTENSIONS);
-   assert (strstr (egl_extension_st, "EGL_KHR_create_context") != NULL);
-   assert (strstr (egl_extension_st, "EGL_KHR_surfaceless_context") != NULL);
+    const char *egl_extension_st = eglQueryString(egl_dpy, EGL_EXTENSIONS);
+    assert(strstr(egl_extension_st, "EGL_KHR_create_context") != NULL);
+    assert(strstr(egl_extension_st, "EGL_KHR_surfaceless_context") != NULL);
 
-   static const EGLint config_attribs[] = {
-      EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT_KHR,
-      EGL_NONE
-   };
-   EGLConfig cfg;
-   EGLint count;
+    static const EGLint config_attribs[] = {
+        EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT_KHR,
+        EGL_NONE};
+    EGLConfig cfg;
+    EGLint count;
 
-   res = eglChooseConfig (egl_dpy, config_attribs, &cfg, 1, &count);
-   assert (res);
+    res = eglChooseConfig(egl_dpy, config_attribs, &cfg, 1, &count);
+    assert(res);
 
-   res = eglBindAPI (EGL_OPENGL_ES_API);
-   assert (res);
+    res = eglBindAPI(EGL_OPENGL_ES_API);
+    assert(res);
 
-   static const EGLint attribs[] = {
-      EGL_CONTEXT_CLIENT_VERSION, 3,
-      EGL_NONE
-   };
-   EGLContext core_ctx = eglCreateContext (egl_dpy,
+    static const EGLint attribs[] = {
+        EGL_CONTEXT_CLIENT_VERSION, 3,
+        EGL_NONE};
+    EGLContext core_ctx = eglCreateContext(egl_dpy,
                                            cfg,
                                            EGL_NO_CONTEXT,
                                            attribs);
-   assert (core_ctx != EGL_NO_CONTEXT);
+    assert(core_ctx != EGL_NO_CONTEXT);
 
-   res = eglMakeCurrent (egl_dpy, EGL_NO_SURFACE, EGL_NO_SURFACE, core_ctx);
-   assert (res);
+    res = eglMakeCurrent(egl_dpy, EGL_NO_SURFACE, EGL_NO_SURFACE, core_ctx);
+    assert(res);
 
-   /* print some compute limits (not strictly necessary) */
-   GLint work_group_count[3] = {0};
-   for (unsigned i = 0; i < 3; i++)
-      glGetIntegeri_v (GL_MAX_COMPUTE_WORK_GROUP_COUNT,
-                       i,
-                       &work_group_count[i]);
-   printf ("GL_MAX_COMPUTE_WORK_GROUP_COUNT: %d, %d, %d\n",
+    /* print some compute limits (not strictly necessary) */
+    GLint work_group_count[3] = {0};
+    for (unsigned i = 0; i < 3; i++)
+        glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT,
+                        i,
+                        &work_group_count[i]);
+    printf("GL_MAX_COMPUTE_WORK_GROUP_COUNT: %d, %d, %d\n",
            work_group_count[0],
            work_group_count[1],
            work_group_count[2]);
 
-   GLint work_group_size[3] = {0};
-   for (unsigned i = 0; i < 3; i++)
-      glGetIntegeri_v (GL_MAX_COMPUTE_WORK_GROUP_SIZE, i, &work_group_size[i]);
-   printf ("GL_MAX_COMPUTE_WORK_GROUP_SIZE: %d, %d, %d\n",
+    GLint work_group_size[3] = {0};
+    for (unsigned i = 0; i < 3; i++)
+        glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, i, &work_group_size[i]);
+    printf("GL_MAX_COMPUTE_WORK_GROUP_SIZE: %d, %d, %d\n",
            work_group_size[0],
            work_group_size[1],
            work_group_size[2]);
 
-   GLint max_invocations;
-   glGetIntegerv (GL_MAX_COMPUTE_WORK_GROUP_INVOCATIONS, &max_invocations);
-   printf ("GL_MAX_COMPUTE_WORK_GROUP_INVOCATIONS: %d\n", max_invocations);
+    GLint max_invocations;
+    glGetIntegerv(GL_MAX_COMPUTE_WORK_GROUP_INVOCATIONS, &max_invocations);
+    printf("GL_MAX_COMPUTE_WORK_GROUP_INVOCATIONS: %d\n", max_invocations);
 
-   GLint mem_size;
-   glGetIntegerv (GL_MAX_COMPUTE_SHARED_MEMORY_SIZE, &mem_size);
-   printf ("GL_MAX_COMPUTE_SHARED_MEMORY_SIZE: %d\n", mem_size);
+    GLint mem_size;
+    glGetIntegerv(GL_MAX_COMPUTE_SHARED_MEMORY_SIZE, &mem_size);
+    printf("GL_MAX_COMPUTE_SHARED_MEMORY_SIZE: %d\n", mem_size);
 
-   /* setup a compute shader */
-   GLuint compute_shader = glCreateShader (GL_COMPUTE_SHADER);
+    /* setup a compute shader */
+    GLuint compute_shader = glCreateShader(GL_COMPUTE_SHADER);
 
-   assert (glGetError () == GL_NO_ERROR);
-   const char *shader_source = COMPUTE_SHADER_SRC;
+    assert(glGetError() == GL_NO_ERROR);
+    const char *shader_source = COMPUTE_SHADER_SRC;
 
-   glShaderSource (compute_shader, 1, &shader_source, NULL);
-   assert (glGetError () == GL_NO_ERROR);
+    glShaderSource(compute_shader, 1, &shader_source, NULL);
+    assert(glGetError() == GL_NO_ERROR);
 
-   glCompileShader (compute_shader);
-   assert (glGetError () == GL_NO_ERROR);
+    glCompileShader(compute_shader);
+    assert(glGetError() == GL_NO_ERROR);
 
-   GLuint shader_program = glCreateProgram ();
+    GLuint shader_program = glCreateProgram();
 
-   glAttachShader (shader_program, compute_shader);
-   assert (glGetError () == GL_NO_ERROR);
+    glAttachShader(shader_program, compute_shader);
+    assert(glGetError() == GL_NO_ERROR);
 
-   glLinkProgram (shader_program);
-   assert (glGetError () == GL_NO_ERROR);
+    glLinkProgram(shader_program);
+    assert(glGetError() == GL_NO_ERROR);
 
-   glDeleteShader (compute_shader);
+    glDeleteShader(compute_shader);
 
-   glUseProgram (shader_program);
-   assert (glGetError () == GL_NO_ERROR);
+    glUseProgram(shader_program);
+    assert(glGetError() == GL_NO_ERROR);
 
-   /* dispatch computation */
-   glDispatchCompute (1, 1, 1);
-   assert (glGetError () == GL_NO_ERROR);
+    /* dispatch computation */
+    glDispatchCompute(1, 1, 1);
+    assert(glGetError() == GL_NO_ERROR);
 
-   printf ("Compute shader dispatched and finished successfully\n");
+    printf("Compute shader dispatched and finished successfully\n");
 
-   /* free stuff */
-   glDeleteProgram (shader_program);
-   eglDestroyContext (egl_dpy, core_ctx);
-   eglTerminate (egl_dpy);
-   gbm_device_destroy (gbm);
-   close (fd);
+    /* free stuff */
+    glDeleteProgram(shader_program);
+    eglDestroyContext(egl_dpy, core_ctx);
+    eglTerminate(egl_dpy);
+    gbm_device_destroy(gbm);
+    close(fd);
 
-   return 0;
+    return 0;
 }
