55d15a741808b7b6b96de42d45990508522a5604
Simon CHENG Ye <cnhzcy14@gmail.com>
2023-07-16 22:40:55 +0800

    update

--- script/printLog.py
+++ script/printLog.py
@@ -4,16 +4,23 @@ import numpy as np
 
 fig, ax = plt.subplots()
 ax.set_title("")
-ax.set_ylim([10, 100])
+ax.set_ylim([0, 300])
 
-log = pd.read_csv('4.csv', header=None)
+csv_file = input('Input a csv file:\n')
 
-# time = 60*log[0].to_numpy() + log[1].to_numpy()
-time = log[2].to_numpy()
+
+log = pd.read_csv(csv_file, header=None)
+
+host_time = (60*log[0].to_numpy() + log[1].to_numpy()) * 1000
+
+device_time_0 = log[4].to_numpy()
+device_time_1 = log[5].to_numpy()
+
+# time = log[2].to_numpy()
 
 # diff = np.diff(time)
 # print(diff)
 # log[1].plot(ax=ax)
 ax.grid()
-plt.plot(time)
+plt.plot(np.diff(host_time, n=1, axis=-1))
 plt.show()
