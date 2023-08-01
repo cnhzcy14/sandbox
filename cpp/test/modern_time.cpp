4a769af1b02500ebcfc869ebbf8f978e5ddf1d2e
cnhzcy14 <cnhzcy14@gmail.com>
2023-08-01 07:39:50 +0000

    update

--- cpp/test/modern_time.cpp
+++ cpp/test/modern_time.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 #include <chrono>
+#include <thread>
+
+namespace sc = std::chrono;
 
 int main()
 {
-    auto start = std::chrono::system_clock::now();
+    auto start = sc::system_clock::now();
     // Some computation here
-    auto end = std::chrono::system_clock::now();
+    sc::milliseconds timespan(1605); // or whatever
+    std::this_thread::sleep_for(timespan);
+    auto end = sc::system_clock::now();
 
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    double elapsed_miliseconds =
+        1.e-3 * sc::duration_cast<sc::microseconds>(end - start).count();
+
+    // std::chrono::duration<double> elapsed_miliseconds = end - start;
+    std::time_t end_time = sc::system_clock::to_time_t(end);
 
     std::cout << "finished computation at " << std::ctime(&end_time)
-              << "elapsed time: " << elapsed_seconds.count() << "s"
+              << "elapsed time: " << elapsed_miliseconds << "ms"
               << std::endl;
 }
