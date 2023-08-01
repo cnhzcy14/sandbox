d9f34ec6c058429fce1d0af2e785c4a6ff277d23
cnhzcy14 <cnhzcy14@gmail.com>
2023-08-01 11:23:52 +0000

    update

--- cpp/test/modern_file.cpp
+++ cpp/test/modern_file.cpp
@@ -1,25 +1,49 @@
-#include <filesystem>
-#include <string>
 #include <iostream>
-
-namespace fs = std::filesystem;
+#include <fstream>
+#include <string>
+#include <vector>
 
 int main()
 {
-    const std::string path = "newF";
-    for (int i = 1; i <= 10; ++i)
+    // The base filename with "%d" representing the index
+    const std::string baseFilename = "/home/radxa/work/data/700/pointsData_/points%03d.txt";
+
+    // // Vector to store the generated filenames
+    // std::vector<std::string> filenames;
+
+    // // Number of files to generate
+    // const int numFiles = 5;
+
+    // for (int i = 0; i < numFiles; ++i) {
+    //     char filenameBuffer[100]; // Adjust buffer size as needed
+    //     std::sprintf(filenameBuffer, baseFilename.c_str(), i);
+    //     filenames.push_back(filenameBuffer);
+    // }
+
+    // // Display the generated filenames
+    // for (const auto& filename : filenames) {
+    //     std::cout << filename << std::endl;
+    // }
+
+    std::ofstream ofs; // output file stream
+    
+    for(size_t i = 0; ;i++)
     {
-        try
+        char filenameBuffer[1024]; // Adjust buffer size as needed
+        std::sprintf(filenameBuffer, baseFilename.c_str(), i);
+
+        std::cout << std::string(filenameBuffer) << std::endl;
+        ofs.open(filenameBuffer, std::fstream::in); // set file cursor at the end
+
+        if (ofs)
         {
-            if (fs::create_directory(path + std::to_string(i)))
-                std::cout << "Created a directory\n";
-            else
-                std::cerr << "Failed to create a directory\n";
+            ofs.close();
         }
-        catch (const std::exception &e)
+        else
         {
-            std::cerr << e.what() << '\n';
+            break;
         }
     }
+
     return 0;
 }
